f4f1b8119078febb6ab182b523611381f8cb8ed1
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-05 17:31:58 +0500

    database

--- src/app.js
+++ src/app.js
@@ -6,13 +6,55 @@ const app = express()
 app.use(express.json())
 
 app.post('/notes', async (req,res)=>{
-   const data = req.body
+   const data = req.body // output object {title,description}
    await noteModel.create({
       title: data.Title,
       description: data.Description
    })
    res.status(201).json({
       message: 'Note Created'
+   })
+})
+
+app.get('/notes', async (req,res)=>{
+  const notes = await noteModel.find() // returns array []
+  res.status(200).json({
+   message: 'Notes Fetched Successfully',
+   notes: notes
+  })
+})
+
+// app.get('/notes', async (req,res)=>{
+//   const notes = await noteModel.findOne({ // return single object{}
+// title: 'Notes_2',
+//   }) 
+//   res.status(200).json({
+//    message: 'Notes Fetched Successfully',
+//    notes: notes
+//   })
+// })
+
+app.delete('/notes/:id',async (req,res)=>{
+   const id = req.params.id
+   await noteModel.findOneAndDelete({
+           _id: id,
+   })
+   res.status(200).json({
+      message: 'Note deleted Successfully'
+   })
+})
+
+app.patch('/notes/:id',async (req,res)=>{
+   const id = req.params.id
+   const description = req.body.Description
+   await noteModel.findOneAndUpdate({
+      _id:id
+   },
+   {
+     description: description
+   })
+   res.status(200).json({
+      message: 'Notes updated Successfully'
    })
 })
 
