180fdcb887d377b0bc71aa369b26823b7a779549
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-05 14:57:02 +0500

    db

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://backend:bslnsl5PtqTFfxuK@backend.5itllud.mongodb.net/halley")
+    await mongoose.connect("mongodb+srv://backend:a9nM9GwR59C4UIt1@backend.5itllud.mongodb.net/halley")
 
 console.log('Connected to DB')
 
